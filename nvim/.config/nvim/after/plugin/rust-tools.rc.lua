1090d41778cb9d4b6bf02551b6cfc6b36d25cb74
Simon Zuluaga <simozuluaga@gmail.com>
2022-09-13 04:48:22 -0500

    Mason vim and futuro colorscheme

--- nvim/.config/nvim/after/plugin/rust-tools.rc.lua
+++ nvim/.config/nvim/after/plugin/rust-tools.rc.lua
@@ -3,7 +3,7 @@ local rt = {
         settings = {
             on_attach = function(_, bufnr)
                 -- Hover actions
-                vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+                vim.keymap.set("n", "<C-s>", rt.hover_actions.hover_actions, { buffer = bufnr })
                 -- Code action groups
                 vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
                 require 'illuminate'.on_attach(client)
