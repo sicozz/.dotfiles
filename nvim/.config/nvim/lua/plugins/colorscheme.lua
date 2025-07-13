30cc6833cf6f2e75b3166d928c7dccc1ffc3b747
Simon Zuluaga <simozuluaga@gmail.com>
2025-07-13 13:28:51 -0500

    kanso-zen yellow customization

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "webhooked/kanso.nvim",
+    "sicozz/kanso.nvim",
     lazy = false,
     priority = 1000,
     opts = function()
@@ -17,8 +17,8 @@ return {
         },
         statementStyle = {},
         typeStyle = { bold = true },
-        transparent = false, -- do not set background color
-        dimInactive = true, -- dim inactive window `:h hl-NormalNC`
+        transparent = true, -- do not set background color
+        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
         terminalColors = true, -- define vim.g.terminal_color_{0,17}
         -- colors = { -- add/modify theme and palette colors
         --   palette = {},
